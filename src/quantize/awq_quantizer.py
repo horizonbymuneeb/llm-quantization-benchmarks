32c84b15e211a08b00f93d7116186335c81346ad
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-06-23 10:13:28 +0500

    Fix race condition in parallel calibration

--- src/quantize/awq_quantizer.py
+++ src/quantize/awq_quantizer.py
@@ -295,3 +295,5 @@ if __name__ == '__main__':
 # Update requirements for torch 2.2.0 [2025-06-17T18:37:00]
 
 # Profile memory bandwidth vs compute bottlenecks [2025-06-23T11:24:25]
+
+# Fix race condition in parallel calibration [2025-06-23T10:13:28]
