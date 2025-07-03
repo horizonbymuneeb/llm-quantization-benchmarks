2558eb3945d49e6b187577eb2575c4598a870850
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-07-03 18:37:30 +0500

    Fix INT4 overflow in weight quantization

--- src/quantize/awq_quantizer.py
+++ src/quantize/awq_quantizer.py
@@ -303,3 +303,5 @@ if __name__ == '__main__':
 # Fix INT4 overflow in weight quantization [2025-06-26T09:05:47]
 
 # Update benchmarking suite with new metrics [2025-06-27T09:40:28]
+
+# Fix INT4 overflow in weight quantization [2025-07-03T18:37:30]
