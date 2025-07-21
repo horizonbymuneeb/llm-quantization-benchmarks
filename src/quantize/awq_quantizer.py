b34d12a11f5660a13071f9b1ad1c514d9a199afc
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-07-21 10:57:34 +0500

    Optimize CUDA graph for repeated inference

--- src/quantize/awq_quantizer.py
+++ src/quantize/awq_quantizer.py
@@ -315,3 +315,5 @@ if __name__ == '__main__':
 # Add support for 3-bit quantization [2025-07-15T19:07:31]
 
 # Add group-wise clipping for Outlier channels [2025-07-19T19:52:00]
+
+# Optimize CUDA graph for repeated inference [2025-07-21T10:57:34]
