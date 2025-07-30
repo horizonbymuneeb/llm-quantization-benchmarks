e2211e8ad89bd6983a89f1eff89d85067da747f3
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-07-30 16:42:45 +0500

    Update README with benchmarking results

--- src/quantize/gguf_exporter.py
+++ src/quantize/gguf_exporter.py
@@ -168,3 +168,5 @@ if __name__ == '__main__':
 # Fix memory leak in benchmark runner [2025-07-21T14:29:00]
 
 # Fix memory leak in benchmark runner [2025-07-22T20:59:33]
+
+# Update README with benchmarking results [2025-07-30T16:42:45]
