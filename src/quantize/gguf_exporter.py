89d7ed01ddd9f7af42842d91497abc9fe6b6e7e4
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-12-11 10:56:50 +0500

    Add unit tests for int4 packing utils

--- src/quantize/gguf_exporter.py
+++ src/quantize/gguf_exporter.py
@@ -252,3 +252,5 @@ if __name__ == '__main__':
 # Update README with benchmarking results [2025-12-04T18:22:09]
 
 # Fix zero-point calculation edge case [2025-12-05T12:09:22]
+
+# Add unit tests for int4 packing utils [2025-12-11T10:56:50]
