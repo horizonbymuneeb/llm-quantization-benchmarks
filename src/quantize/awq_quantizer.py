42154faf4d93cb704a6f7c7607549b1552febe8f
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-12-17 15:26:50 +0500

    Optimize CUDA graph for repeated inference

--- src/quantize/awq_quantizer.py
+++ src/quantize/awq_quantizer.py
@@ -383,3 +383,5 @@ if __name__ == '__main__':
 # Fix memory leak in benchmark runner [2025-12-17T11:12:58]
 
 # Add support for 3-bit quantization [2025-12-17T19:31:19]
+
+# Optimize CUDA graph for repeated inference [2025-12-17T15:26:50]
