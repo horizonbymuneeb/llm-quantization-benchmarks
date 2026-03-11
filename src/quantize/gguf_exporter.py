ce5252ec85d8bbd1ca993806966d0d46c63c9025
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-03-11 16:11:29 +0500

    Update requirements for torch 2.2.0

--- src/quantize/gguf_exporter.py
+++ src/quantize/gguf_exporter.py
@@ -300,3 +300,5 @@ if __name__ == '__main__':
 # Add support for custom quantization config [2026-03-05T20:31:26]
 
 # Add support for custom quantization config [2026-03-06T11:22:12]
+
+# Update requirements for torch 2.2.0 [2026-03-11T16:11:29]
